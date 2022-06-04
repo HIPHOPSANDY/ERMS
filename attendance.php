a1fab360559fc966f63485a9286b30efed766844
HIPHOPSANDY <santhoshkumar24cs@gamil.com>
2022-06-04 18:33:50 +0530

    project end

---
+++ attendance.php
@@ -0,0 +1,108 @@
+<?php require_once(dirname(__FILE__) . '/config.php'); 
+if ( !isset($_SESSION['Admin_ID']) || $_SESSION['Login_Type'] != 'admin' ) {
+   	header('location:' . BASE_URL);
+} ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+	<title>Attendance - Payroll</title>
+
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>plugins/datatables/dataTables.bootstrap.css">
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>plugins/datatables/jquery.dataTables_themeroller.css">
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/AdminLTE.css">
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>dist/css/skins/_all-skins.min.css">
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+	<div class="wrapper">
+		
+		<?php require_once(dirname(__FILE__) . '/partials/topnav.php'); ?>
+
+		<?php require_once(dirname(__FILE__) . '/partials/sidenav.php'); ?>
+
+		<div class="content-wrapper">
+			<section class="content-header">
+				<?php 
+
+		// $NewDate=Date('y:m:d', strtotime('+1 days'));
+		// $str = $NewDate;
+		// $timestamp = strtotime($str);
+		
+		// echo $timestamp;
+		
+		// 		//  set_time_limit($timestamp);
+		// 	// $i=$timestamp;
+
+		// set_time_limit(20);
+
+// while ($i<=10)
+// {
+//         echo "i=$i ";
+//         // sleep(100);
+//         $i++;
+// }				
+				
+				
+				
+				 ?>
+				
+				<h1> 
+					<!-- <?php echo $NewDate ?> -->
+					Attendance
+				</h1>
+				
+			</section>
+
+			<section class="content">
+				<div class="row">
+        			<div class="col-xs-12">
+						<div class="box">
+							<div class="box-header">
+								<h3 class="box-title">Employee Attendance</h3>
+							</div>
+							<div class="box-body">
+								<div class="table-responsiove">
+									<table id="attendance" class="table table-bordered table-striped">
+										<thead>
+											<tr>
+												<th>DATE</th>
+												<th>EMP CODE</th>
+												<th>NAME</th>
+												<th>PUNCH-IN</th>
+												<th>PUNCH-OUT</th>
+												<th>WORK HOURS</th>
+											</tr>
+										</thead>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+
+		
+	</div>
+
+	<script src="<?php echo BASE_URL; ?>plugins/jQuery/jquery-2.2.3.min.js"></script>
+	<script src="<?php echo BASE_URL; ?>bootstrap/js/bootstrap.min.js"></script>
+	<script src="<?php echo BASE_URL; ?>plugins/datatables/jquery.dataTables.min.js"></script>
+	<script src="<?php echo BASE_URL; ?>plugins/datatables/dataTables.bootstrap.min.js"></script>
+	<script src="<?php echo BASE_URL; ?>plugins/bootstrap-notify/bootstrap-notify.min.js"></script>
+	<script src="<?php echo BASE_URL; ?>dist/js/app.min.js"></script>
+	<script type="text/javascript">var baseurl = '<?php echo BASE_URL; ?>';</script>
+	<script src="<?php echo BASE_URL; ?>dist/js/script.js?rand=<?php echo rand(); ?>"></script>
+</body>
+</html>
